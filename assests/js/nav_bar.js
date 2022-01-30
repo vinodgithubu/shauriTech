3f6b9b21403be091df093e181c3e754eacd6f64f
vinodkumar <attatvinod@gmail.com>
2022-01-30 21:28:22 +0530

    theme and home page img change

--- assests/js/nav_bar.js
+++ assests/js/nav_bar.js
@@ -1,12 +1,14 @@
- $(document).ready(function(){
+var color_020101 = "#232223"; //"#020101";
+
+$(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 120) {      
-	    $(".topnav-wrapper").css("background-color" , "#020101");
-	    $(".topnav").css("background-color" , "#020101");
+	    $(".topnav-wrapper").css("background-color" , color_020101);
+	    // $(".topnav").css("background-color" , color_020101);
 	  } else{
 	    $(".topnav-wrapper").css("background-color" , "transparent");
-		  $(".topnav").css("background-color" , "transparent");  	
+		  // $(".topnav").css("background-color" , "transparent");  	
 	  }
   })
 })
