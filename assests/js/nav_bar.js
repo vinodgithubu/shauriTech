dd9fe0ea9a7f40c548441776fa89698ffd66bd46
vinodkumar <attatvinod@gmail.com>
2022-02-05 22:32:23 +0530

    baking, media, motor ..pages

--- assests/js/nav_bar.js
+++ assests/js/nav_bar.js
@@ -21,3 +21,14 @@ function navBarFun() {
     }
 }
   
+
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+      if (pos < winTop + 10 ) {
+        $(this).addClass("slide");
+      }
+  });
+});
